f0cd9cd855137cafa50fa05cf4d05560cd2b844d
David <sanderdj90@gmail.com>
2021-12-17 12:46:06 -0700

    remove seo file for build

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,8 @@
 import React, { FunctionComponent } from 'react'
-
 import Layout from '../components/Layout'
-import Seo from '../components/Seo'
 
 const NotFoundPage: FunctionComponent = () => (
   <Layout>
-    <Seo title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist...</p>
   </Layout>
