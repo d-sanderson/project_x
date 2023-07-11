6d42a146af11c1960baed16892ec9fa14acf0e1a
David <sanderdj90@gmail.com>
2023-07-11 01:12:47 -0600

    project card updates

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,6 +10,27 @@ interface Props {
   repo: string
   url: string
 }
+
+const hoverStyles = {
+  backgroundSize: '100% 100%',
+  backgroundPosition: '0 100%',
+}
+
+const anchorStyles = {
+  color: 'inherit',
+  textDecoration: 'none',
+  backgroundImage: 'linear-gradient(120deg,#f36aff,#f36aff);',
+  backgroundSize: '100% 0.1em',
+  backgroundPosition: '0 90%',
+  backgroundRepeat: 'no-repeat',
+  transition: 'background-size 200ms linear',
+}
+
+const buttonStyles = {
+  ...anchorStyles,
+  borderRadius: '20px',
+}
+
 const ProjectCard: FunctionComponent<Props> = ({
   title, tags, description, repo, url,
 }: Props) => (
@@ -34,34 +55,47 @@ const ProjectCard: FunctionComponent<Props> = ({
     <Box
       sx={{
         a: {
-          color: 'inherit',
-          textDecoration: 'none',
-          backgroundImage: 'linear-gradient(120deg,#f36aff,#f36aff);',
-          backgroundSize: '100% 0.1em',
-          backgroundPosition: '0 90%',
-          backgroundRepeat: 'no-repeat',
-          transition: 'background-size 200ms linear',
+          ...anchorStyles,
         },
         'a:hover': {
-          backgroundSize: '100% 100%',
-          backgroundPosition: '0 100%',
+          ...hoverStyles,
         },
       }}
+      my={2}
       dangerouslySetInnerHTML={{ __html: description }}
     />
-    <Flex sx={{ justifyContent: 'flex-end' }}>
+    <Flex sx={{ justifyContent: 'flex-end' }} my={3}>
       {url && (
         <Link target="_blank" rel="noopenner noreferrer" href={url}>
-          <Button p={1} as="a" my={2} mx={1}>
+          <Box
+            p={1}
+            as="a"
+            my={2}
+            mx={1}
+            sx={{
+              '&': { ...buttonStyles, backgroundImage: 'linear-gradient(120deg,#43f4ff,#43f4ff)' },
+              '&:hover': { ...hoverStyles },
+            }}
+          >
             See Live
-          </Button>
+          </Box>
         </Link>
       )}
       {repo && (
         <Link target="_blank" rel="noopenner noreferrer" href={repo}>
-          <Button p={1} my={2} mx={1} as="a" variant="secondary">
+          <Box
+            p={1}
+            my={2}
+            mx={1}
+            as="a"
+            variant="secondary"
+            sx={{
+              '&': { ...buttonStyles, backgroundImage: 'linear-gradient(120deg,#f36aff,#f36aff)' },
+              '&:hover': { ...hoverStyles },
+            }}
+          >
             View Source
-          </Button>
+          </Box>
         </Link>
       )}
     </Flex>
